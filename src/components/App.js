c21ca9ed60be05727b9459176d1f67ed493bb18e
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-31 03:24:59 +0530

    sdsd

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <div>
         <button className="increment" onClick={()=>dispatch(increment())}>increment</button>
-        <button className="decrement" onClick={()=>dispatch(decrement())}>decrement</button>
+        <button className="decrement" onClick={()=>dispatch(decrement())}>increment</button>
         <h1 className="output">{x}</h1>
     </div>
   )
